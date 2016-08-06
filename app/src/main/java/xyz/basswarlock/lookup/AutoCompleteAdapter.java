4a839bc323561fcc244bf0cebf2b583ee8116a38
Zhang Chuanyi <public_zcy@163.com>
2016-08-06 15:52:52 -0500

    Change appearance of button, Using Jsoup, Using Spanned Class.

---
+++ app/src/main/java/xyz/basswarlock/lookup/AutoCompleteAdapter.java
@@ -0,0 +1,76 @@
+package xyz.basswarlock.lookup;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+    private ArrayList<String> mData;
+
+    public AutoCompleteAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+        mData = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount(){
+        return mData.size();
+    }
+
+    @Override
+    public String getItem(int index){
+        return mData.get(index);
+    }
+
+    @Override
+    public Filter getFilter(){
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (null != constraint) {
+                    mData.clear();
+                    String url = "https://www.vocabulary.com/dictionary/autocomplete?search="
+                            + constraint;
+                    try {
+                        Document doc = Jsoup.connect(url).get();
+                        Elements words = doc.select("li > div > span.word");
+                        for (Element word : words)
+                        {
+                            mData.add(word.text());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    filterResults.values = mData;
+                    filterResults.count = mData.size();
+                } else {
+                    filterResults.values = mData;
+                    filterResults.count = mData.size();
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults filterResults) {
+                if (null != filterResults && filterResults.count > 0) {
+                    notifyDataSetChanged();
+                } else {
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+    }
+
+
+}
--- app/src/main/java/xyz/basswarlock/lookup/MainActivity.java
+++ app/src/main/java/xyz/basswarlock/lookup/MainActivity.java
@@ -8,29 +8,23 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Selector;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
+    AutoCompleteTextView autoCompleteTextView;
     TextView textView;
 
     @Override
@@ -38,16 +32,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoComplete);
+        autoCompleteTextView.setAdapter(new AutoCompleteAdapter(this,
+                android.R.layout.simple_dropdown_item_1line));
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                searchHandler(view);
+            }
+        });
         textView = (TextView) findViewById(R.id.textView);
         textView.setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void searchHandler(View view) {
-        String message = editText.getText().toString().trim();
+        String message = autoCompleteTextView.getText().toString().trim();
         String url = "https://vocabulary.com/dictionary/definition.ajax?search="
                 + message + "&lang=en";
-        Log.v("searchHandler", message);
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
@@ -74,12 +76,12 @@ public class MainActivity extends AppCompatActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 for (String str : result) {
                     spanned = (Spanned) TextUtils.concat(spanned,
-                            Html.fromHtml(str, Html.FROM_HTML_MODE_LEGACY), "\n\n");
+                            Html.fromHtml(str+"<br/><br/>", Html.FROM_HTML_MODE_LEGACY));
                 }
             } else {
                 for (String str : result) {
                     spanned = (Spanned) TextUtils.concat(spanned,
-                            Html.fromHtml(str), "\n");
+                            Html.fromHtml(str+"<br/><br/>"));
                 }
             }
             SpannableString text = new SpannableString(spanned);
@@ -94,18 +96,7 @@ public class MainActivity extends AppCompatActivity {
         if (null != defShort && null != defLong) {
             return new String[] {defShort.html(), defLong.html()};
         } else {
-            return new String[] {""};
+            return new String[] {"Not found."};
         }
-    }
-
-    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
-        Reader reader = new InputStreamReader(stream, "UTF-8");
-        char[] buffer = new char[256];
-        String result = "";
-        while (-1 != reader.read(buffer)) {
-            result += new String(buffer);
-        }
-        result += new String(buffer);
-        return result;
     }
 }
